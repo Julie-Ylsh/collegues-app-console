316ac94e877d54dfc31bff1764cb41f18c1528f4
julie <julie.bouaye@hotmail.fr>
2019-05-09 17:23:08 +0200

    Migration pour security boot

--- service.ts
+++ service.ts
@@ -1,10 +1,22 @@
 import request from 'request-promise-native';
 import * as metiers from './domains';
 
+let rq = request.defaults({jar: true})
+
+rq({
+    url: "https://julie-collegue-api.herokuapp.com/auth",
+    method: 'POST',
+    json: true,
+    body: {
+        "matriculeCollegue" : "1",
+        "motDePasse" : "pass1"
+    }
+})
+
 
 export function rechercherColleguesParNom(nomRecherche:string) {
     let URL = `https://julie-collegue-api.herokuapp.com/collegue?nomClient=${nomRecherche}`
-    return request(URL, { json: true }).promise();
+    return rq(URL, { json: true }).promise();
 };
 
 export function creerUnCollegue(collegue:metiers.Collegue) {
